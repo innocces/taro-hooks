4be73ba05c67c8f19732b3df17876d0a424f6080
innocces <18435105624@163.com>
2021-06-29 23:59:06 +0800

    fix(radio): fix systemInfo demo value bug

--- packages/app/src/pages/useSystemInfo/index.tsx
+++ packages/app/src/pages/useSystemInfo/index.tsx
@@ -7,22 +7,26 @@ import { getSystemInfoSync } from '@tarojs/taro';
 
 import 'taro-ui/dist/style/components/icon.scss';
 
+const MOCK = '1';
+
 const transferOptions = (systemInfo: getSystemInfoSync.Result) =>
   Object.keys(systemInfo).map((key: any) => ({
     label: key + ':',
     value: key,
-    desc: JSON.stringify(systemInfo[key as keyof getSystemInfoSync.Result]),
+    desc: JSON.stringify(
+      systemInfo[key as keyof getSystemInfoSync.Result] || '',
+    ),
   }));
 
 export default () => {
   const systemInfo = useSystemInfo();
-  console.log(systemInfo);
+
   return (
     <>
       <DocPage title="useSystemInfo 系统信息" panelTitle="useSystemInfo">
         <AtRadio
           options={transferOptions(systemInfo)}
-          value={1}
+          value={MOCK}
           onClick={console.log}
         />
       </DocPage>
