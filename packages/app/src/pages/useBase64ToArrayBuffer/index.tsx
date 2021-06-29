7ef432c9522b9c4ee02e64ba4ae4e573b85fb69b
innocces <18435105624@163.com>
2021-06-29 21:09:08 +0800

    fix(base64 demo): fix base64 demo input error

--- packages/app/src/pages/useBase64ToArrayBuffer/index.tsx
+++ packages/app/src/pages/useBase64ToArrayBuffer/index.tsx
@@ -13,12 +13,14 @@ import './index.less';
 
 export default () => {
   const [base64, setBase64] = useState('');
+  const [inputValue, setInputValue] = useState('');
   const buffer = useBase64ToArrayBuffer(base64);
 
   const translateArrayBuffer = useCallback((value) => {
     const arrayBuffer = encode(value);
     setBase64(arrayBuffer);
-    console.log(arrayBuffer, useBase64ToArrayBuffer(arrayBuffer));
+    setInputValue(value);
+    return value;
   }, []);
 
   return (
@@ -30,8 +32,9 @@ export default () => {
         <AtInput
           title="待转换文本"
           type="text"
+          value={inputValue}
           placeholder="此处输入文本会自动转换为base64"
-          onChange={(value) => translateArrayBuffer(value)}
+          onChange={translateArrayBuffer}
         />
         <View className="at-article__p">当前转换值base64为: </View>
         <View className="at-article__p use-base64-to-array-buffer">
