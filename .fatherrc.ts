858fa52fc0b44adfa133fbe764458d2faf02a546
innocces <18435105624@163.com>
2021-07-25 13:59:58 +0800

    ci(config): fix some config of editor

--- .fatherrc.ts
+++ .fatherrc.ts
@@ -1,15 +1,21 @@
 export default {
   esm: {
     type: 'babel',
+    file: 'taro-hooks',
+    mjs: true,
+    minify: true,
   },
   cjs: {
     type: 'babel',
+    file: 'taro-hooks',
     lazy: true,
+    minify: true,
   },
   umd: {
     globals: 'taro-hooks',
     name: 'taro-hooks',
     miniFile: true,
+    sourcemap: true,
   },
   runtimeHelpers: true,
 };
