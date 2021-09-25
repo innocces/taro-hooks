a133b55b00492f02a59b27fdd4926fba246e3ff2
innocces <18435105624@163.com>
2021-09-25 18:10:35 +0800

    build(postinstall): postinstall console message add

---
+++ packages/hooks/postinstall.js
@@ -0,0 +1,7 @@
+var BANNER =
+  '\u001B[96mThank you for using taro-hooks (\u001B[94m https://github.com/innocces/taro-hooks \u001B[96m) for taro hook library!\u001B[0m\n\n' +
+  '\u001B[96mPlease consider give me a ⭐️ ! \u001B[0m\n' +
+  '\u001B[96m>\u001B[94m https://opencollective.com/taro-hooks \u001B[0m\n' +
+  '\u001B[96m>\u001B[94m https://www.buymeacoffee.com/innocces \u001B[0m\n' +
+  '\u001B[96mAlso, the author of taro-hooks (\u001B[94m https://github.com/innocces \u001B[96m) is looking for a good job -)\u001B[0m\n';
+console.log(BANNER);
