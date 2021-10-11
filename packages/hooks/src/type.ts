fd9adf9f2fad764cd7e851bdab990293cb6ecade
innocces <jiangjiangjiangxi@gmail.com>
2021-10-11 14:41:18 +0800

    feat(useble): add useBLE

--- packages/hooks/src/type.ts
+++ packages/hooks/src/type.ts
@@ -5,3 +5,7 @@ export type TRecord = { [_: string]: any };
 export type TAuthResultType = 'accept' | 'reject' | 'ban';
 
 export type TNormalAction<T = General.CallbackResult> = () => Promise<T>;
+
+export type TGeneralCallback<T = General.CallbackResult, R = void> = (
+  callbackResult: T,
+) => R;
