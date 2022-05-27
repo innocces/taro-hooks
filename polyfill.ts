e153459c79a2a4f7bec2d9f53e612b039944b43e
innocces <jiangjiangjiangxi@gmail.com>
2022-05-27 21:18:49 +0800

    chore(trash): remove useless files

--- polyfill.ts
+++
@@ -1,5 +0,0 @@
-// add some poly to fix dumi -> taro
-import Taro, { eventCenter } from '@tarojs/taro';
-
-// runtime change due to atmessage delay
-Taro.atMessage = eventCenter.trigger.bind(eventCenter, 'atMessage');
