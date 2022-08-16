5faf6c7899bd6d63d4e0db988fc696e079c98d5d
innocces <jiangjiangjiangxi@gmail.com>
2022-08-16 23:11:40 +0800

    feat(usetabbar): refactor useTabBar hooks

--- packages/hooks/src/useTabBar/index.md
+++ packages/hooks/src/useTabBar/index.md
@@ -19,14 +19,8 @@ group:
 
 ## API
 
-```jsx | pure
-const {
-  setTabBarVisible,
-  setRedDotVisible,
-  setBadgeVisible,
-  setTabBarItem,
-  setTabBarStyle,
-} = useTabBar();
+```ts
+const { toggleRedDot, toggleBadge, setStyle, setItem, toggle } = useTabBar();
 ```
 
 ## 参数说明
@@ -35,58 +29,20 @@ const {
 
 ## 返回值说明
 
-| 参数             | 类型                                                                                           | 说明                                                                   |
-| ---------------- | ---------------------------------------------------------------------------------------------- | ---------------------------------------------------------------------- |
-| setTabBarVisible | `(visible: boolean, animation?: boolean) => Promise<General.CallbackResult>`                   | 显示/隐藏 tabBar                                                       |
-| setRedDotVisible | `(visible: boolean, indexList: number[]) => Promise<General.CallbackResult>`                   | 显示/隐藏 tabBar 某几项的右上角的红点                                  |
-| setBadgeVisible  | `(indexList: number[] &#124; ISetBadgeVisibleOptionItem[]) => Promise<General.CallbackResult>` | 为 tabBar 某几项的右上角添加/移除文本(移除时仅需要传递移除`index`即可) |
-| setTabBarItem    | `(itemList: ISetTabBarItemOption[]) => Promise<General.CallbackResult>`                        | 动态设置 tabBar 某几项的内容                                           |
-| setTabBarStyle   | `(option: ISetTabBarStyleOption) => Promise<General.CallbackResult>`                           | 动态设置 tabBar 的整体样式                                             |
-
-### ISetBadgeVisibleOptionItem
-
-| 参数  | 类型     | 说明                                  |
-| ----- | -------- | ------------------------------------- |
-| index | `number` | tabBar 的哪一项，从左边算起           |
-| text  | `string` | 显示的文本，超过 4 个字符则显示成 ... |
-
-### ISetTabBarItemOption
-
-| 参数             | 类型     | 说明                                                                                               |
-| ---------------- | -------- | -------------------------------------------------------------------------------------------------- |
-| index            | `number` | tabBar 的哪一项，从左边算起                                                                        |
-| text             | `string` | tab 上的按钮文字                                                                                   |
-| iconPath         | `string` | 图片路径，icon 大小限制为 40kb，建议尺寸为 81px \* 81px，当 postion 为 top 时，此参数无效          |
-| selectedIconPath | `string` | 选中时的图片路径，icon 大小限制为 40kb，建议尺寸为 81px \* 81px ，当 postion 为 top 时，此参数无效 |
-
-### ISetTabBarStyleOption
-
-| 值              | 类型     | 说明                                     |
-| --------------- | -------- | ---------------------------------------- |
-| color           | `string` | tab 上的文字默认颜色，HexColor           |
-| selectedColor   | `string` | tab 上的文字选中时的颜色，HexColor       |
-| backgroundColor | `string` | tab 的背景色，HexColor                   |
-| borderStyle     | `string` | tabBar 上边框的颜色， 仅支持 black/white |
+| 参数         | 类型                                                                                                                   | 说明                                                                 |
+| ------------ | ---------------------------------------------------------------------------------------------------------------------- | -------------------------------------------------------------------- |
+| toggle       | `(animation?: boolean) => Promise<General.CallbackResult>`                                                             | 显示/隐藏 tabBar                                                     |
+| toggleRedDot | `(index?: number) => Promise<General.CallbackResult>`                                                                  | 显示/隐藏 tabBar 某项的右上角的红点                                  |
+| toggleBadge  | `(index?: number, text?: string) => Promise<General.CallbackResult>`                                                   | 为 tabBar 某项的右上角添加/移除文本(移除时仅需要传递移除`index`即可) |
+| setItem      | `(option: Omit<ExcludeOption<Taro.setTabBarItem.Option>, 'index'>, index?: number) => Promise<General.CallbackResult>` | 动态设置 tabBar 某项的内容(若不传`index`则会统一给所有的添加样式)    |
+| setStyle     | `(option: ExcludeOption<Taro.setTabBarStyle.Option>) => Promise<General.CallbackResult>`                               | 动态设置 tabBar 的整体样式                                           |
 
 ## 代码演示
 
-<code src="@pages/useTabBar" />
+<code src="useTabBar/index" group="layout" />
 
 ## Hook 支持度
 
 | 微信小程序 | H5  | ReactNative |
 | :--------: | :-: | :---------: |
 |     ✔️     | ✔️  |     ✔️      |
-
-## FAQ
-
-### 1. 更多说明
-
-- [showTabBar](https://developers.weixin.qq.com/miniprogram/dev/api/ui/tab-bar/wx.showTabBar.html)
-- [hideTabBar](https://developers.weixin.qq.com/miniprogram/dev/api/ui/tab-bar/wx.hideTabBar.html)
-- [showTabBarRedDot](https://developers.weixin.qq.com/miniprogram/dev/api/ui/tab-bar/wx.showTabBarRedDot.html)
-- [hideTabBarRedDot](https://developers.weixin.qq.com/miniprogram/dev/api/ui/tab-bar/wx.hideTabBarRedDot.html)
-- [setTabBarBadge](https://developers.weixin.qq.com/miniprogram/dev/api/ui/tab-bar/wx.setTabBarBadge.html)
-- [removeTabBarBadge](https://developers.weixin.qq.com/miniprogram/dev/api/ui/tab-bar/wx.removeTabBarBadge.html)
-- [setTabBarItem](https://developers.weixin.qq.com/miniprogram/dev/api/ui/tab-bar/wx.setTabBarItem.html)
-- [setTabBarStyle](https://developers.weixin.qq.com/miniprogram/dev/api/ui/tab-bar/wx.setTabBarStyle.html)
--- packages/hooks/src/useTabBar/index.ts
+++ packages/hooks/src/useTabBar/index.ts
@@ -7,201 +7,125 @@ import {
   setTabBarBadge,
   removeTabBarBadge,
   setTabBarItem,
+  useTaroRef,
 } from '@tarojs/taro';
-import { typeOf } from '../utils/tool';
-import { useCallback } from 'react';
+import { isNumber, isObject } from '@taro-hooks/shared';
+import usePromise from '../usePromise';
+import useApp from '../useApp';
+import { generateGeneralFail } from '../utils/tool';
+import type {
+  PromiseAction,
+  PromiseOptionalAction,
+  PromiseParamsAction,
+  ExcludeOption,
+  RecordData,
+} from '../type';
 
-export type Result = TaroGeneral.CallbackResult & { data?: unknown };
+export type ToggleRedDot = PromiseAction<number>;
 
-export type TSetTabBarVisible = (
-  visible: boolean,
-  animation?: boolean,
-) => Promise<TaroGeneral.CallbackResult>;
+export type Toggle = PromiseOptionalAction<boolean>;
 
-export type TSetRedDotVisible = (
-  visible: boolean,
-  indexList: number[],
-) => Promise<Result>;
+export type ToggleBadge = PromiseParamsAction<
+  (index: number, text?: string) => void
+>;
 
-export interface ISetBadgeVisibleOptionItem {
-  index: number;
-  text: string;
-}
-export type TSetBadgeVisible = (
-  indexList: number[] | ISetBadgeVisibleOptionItem[],
-) => Promise<Result>;
+export type SetStyle = PromiseOptionalAction<
+  ExcludeOption<Taro.setTabBarStyle.Option>
+>;
 
-export interface ISetTabBarItemOption {
-  index: number;
-  iconPath?: string;
-  selectedIconPath?: string;
-  text?: string;
-}
-export type TSetTabBarItem = (
-  itemList: ISetTabBarItemOption[],
-) => Promise<Result>;
-
-export interface ISetTabBarStyleOption {
-  backgroundColor?: string;
-  borderStyle?: 'black' | 'white';
-  color?: string;
-  selectedColor?: string;
-}
-export type TSetTabBarStyle = (
-  option: ISetTabBarStyleOption,
-) => Promise<TaroGeneral.CallbackResult>;
+export type SetItem = PromiseParamsAction<
+  (
+    option: Omit<ExcludeOption<Taro.setTabBarItem.Option>, 'index'>,
+    index?: number,
+  ) => void
+>;
 
 function useTabBar(): {
-  setTabBarVisible: TSetTabBarVisible;
-  setRedDotVisible: TSetRedDotVisible;
-  setBadgeVisible: TSetBadgeVisible;
-  setTabBarItem: TSetTabBarItem;
-  setTabBarStyle: TSetTabBarStyle;
+  toggleRedDot: ToggleRedDot;
+  toggleBadge: ToggleBadge;
+  toggle: Toggle;
+  setStyle: SetStyle;
+  setItem: SetItem;
 } {
-  const setTabBarVisible = useCallback<TSetTabBarVisible>(
-    (visible = false, animation = false) => {
-      return new Promise((resolve, reject) => {
-        if (typeof visible !== 'boolean') {
-          reject({ errMsg: 'setTabBarVisible: fail' });
-        } else {
-          try {
-            const setTabBarVisibleMethod = visible ? showTabBar : hideTabBar;
-            setTabBarVisibleMethod({
-              animation,
-              success: resolve,
-              fail: reject,
-            });
-          } catch (e) {
-            reject({ errMsg: 'setTabBarVisible: fail', data: e });
-          }
-        }
+  const memoRedDot = useTaroRef<RecordData<string, boolean | undefined>>({});
+  const memoVisible = useTaroRef<boolean>(true);
+  const { app } = useApp();
+
+  const showRedDotAsync =
+    usePromise<ExcludeOption<Taro.showTabBarRedDot.Option>>(showTabBarRedDot);
+  const hideRedDotAsync =
+    usePromise<ExcludeOption<Taro.hideTabBarRedDot.Option>>(hideTabBarRedDot);
+
+  const toggleRedDot: ToggleRedDot = (index) => {
+    // first is undefined. so need set visible
+    const memoIndexRedDotState = memoRedDot.current[index];
+    const toggleAction = memoIndexRedDotState
+      ? hideRedDotAsync
+      : showRedDotAsync;
+    return toggleAction({ index }).then((res) => {
+      memoRedDot.current[index] = !memoIndexRedDotState;
+      return res;
+    });
+  };
+
+  const showBadgeAsync =
+    usePromise<ExcludeOption<Taro.setTabBarBadge.Option>>(setTabBarBadge);
+  const hideBadgeAsync =
+    usePromise<ExcludeOption<Taro.removeTabBarBadge.Option>>(removeTabBarBadge);
+
+  const toggleBadge: ToggleBadge = (index: number, text?: string) => {
+    if (text?.length) return showBadgeAsync({ index, text });
+    return hideBadgeAsync({ index });
+  };
+
+  const showTabBarAsync =
+    usePromise<ExcludeOption<Taro.showTabBar.Option>>(showTabBar);
+  const hideTabBarAsync =
+    usePromise<ExcludeOption<Taro.hideTabBar.Option>>(hideTabBar);
+
+  const toggle: Toggle = (animation) => {
+    const toggleAction = memoVisible.current
+      ? hideTabBarAsync
+      : showTabBarAsync;
+    return toggleAction({ animation }).then((res) => {
+      memoVisible.current = !memoVisible.current;
+      return res;
+    });
+  };
+
+  const setStyle: SetStyle =
+    usePromise<ExcludeOption<Taro.setTabBarStyle.Option>>(setTabBarStyle);
+
+  const setItemAsync =
+    usePromise<ExcludeOption<Taro.setTabBarItem.Option>>(setTabBarItem);
+
+  const setItem: SetItem = (option, index) => {
+    if (isObject(option) && isNumber(index)) {
+      return setItemAsync({ ...option, index });
+    }
+    // if not index, set all tabbbaritem
+    const tabbarList = app?.config?.tabBar?.list;
+    if (tabbarList?.length) {
+      return Promise.all(
+        tabbarList.map((v, i: number) => {
+          return setItemAsync({ ...option, index: i });
+        }),
+      ).then(([res]) => {
+        return res;
       });
-    },
-    [],
-  );
-  const setRedDotVisible = useCallback<TSetRedDotVisible>(
-    (visible, indexList = []) => {
-      return new Promise(async (resolve, reject) => {
-        const validIndexList =
-          indexList.filter((v) => typeof v === 'number') || [];
-        if (
-          !Array.isArray(indexList) ||
-          !validIndexList.length ||
-          typeof visible !== 'boolean'
-        ) {
-          reject({ errMsg: 'setRedDotVisible: fail' });
-        } else {
-          try {
-            const setRedDotVisibleMethod = visible
-              ? showTabBarRedDot
-              : hideTabBarRedDot;
-            const successIndexList = [];
-            for await (let index of validIndexList) {
-              try {
-                await setRedDotVisibleMethod({ index });
-                successIndexList.push(index);
-              } catch (e) {
-                console.log({
-                  errMsg: `setRedDotVisible-${index}: fail`,
-                  data: e,
-                });
-              }
-            }
-            resolve({ errMsg: 'setRedDotVisible: ok', data: successIndexList });
-          } catch (e) {
-            reject({ errMsg: 'setRedDotVisible: fail', data: e });
-          }
-        }
-      });
-    },
-    [],
-  );
-  const setBadgeVisible = useCallback<TSetBadgeVisible>((indexList = []) => {
-    return new Promise(async (resolve, reject) => {
-      const isSet = (indexList as []).every((v) => typeof v !== 'number');
-      const validIndexList = (indexList as []).filter((v) =>
-        !isSet ? typeof v === 'number' : typeOf(v, 'Object'),
-      );
-      if (!Array.isArray(indexList) || !validIndexList.length) {
-        reject({ errMsg: 'setBadgeVisible: fail' });
-      } else {
-        try {
-          const successIndexList = [];
-          for await (let indexOrOption of validIndexList) {
-            const index = isSet
-              ? (indexOrOption as ISetBadgeVisibleOptionItem).index
-              : indexOrOption;
-            try {
-              if (isSet) {
-                await setTabBarBadge({
-                  ...(indexOrOption as ISetBadgeVisibleOptionItem),
-                });
-              } else {
-                await removeTabBarBadge({ index: indexOrOption });
-              }
-              successIndexList.push(index);
-            } catch (e) {
-              console.log({
-                errMsg: `setBadgeVisible-${index}: fail`,
-                data: e,
-              });
-            }
-          }
-          resolve({ errMsg: 'setBadgeVisible: ok', data: successIndexList });
-        } catch (e) {
-          reject({ errMsg: 'setBadgeVisible: fail', data: e });
-        }
-      }
-    });
-  }, []);
-  const setTabBarItemAsync = useCallback<TSetTabBarItem>((itemList = []) => {
-    return new Promise(async (resolve, reject) => {
-      const validIndexList =
-        itemList.filter(
-          (v) => typeOf(v, 'Object') && typeof v.index === 'number',
-        ) || [];
-      if (!Array.isArray(itemList) || !validIndexList.length) {
-        reject({ errMsg: 'setTabBarItem: fail' });
-      } else {
-        try {
-          const successIndexList = [];
-          for await (let singleOption of validIndexList) {
-            try {
-              await setTabBarItem({ ...singleOption });
-              successIndexList.push(singleOption.index);
-            } catch (e) {
-              console.log({
-                errMsg: `setTabBarItem-${singleOption.index}: fail`,
-                data: e,
-              });
-            }
-          }
-          resolve({ errMsg: 'setTabBarItem: ok', data: successIndexList });
-        } catch (e) {
-          reject({ errMsg: 'setTabBarItem: fail', data: e });
-        }
-      }
-    });
-  }, []);
-  const setTabBarStyleAsync = useCallback<TSetTabBarStyle>((option = {}) => {
-    return new Promise((resolve, reject) => {
-      try {
-        setTabBarStyle({
-          ...option,
-          success: resolve,
-          fail: reject,
-        });
-      } catch (e) {
-        reject({ errMsg: 'setTabBarStyle: fail', data: e });
-      }
-    });
-  }, []);
+    }
+
+    return Promise.reject(
+      generateGeneralFail('setItem', 'you must set config'),
+    );
+  };
+
   return {
-    setTabBarVisible,
-    setRedDotVisible,
-    setBadgeVisible,
-    setTabBarItem: setTabBarItemAsync,
-    setTabBarStyle: setTabBarStyleAsync,
+    toggleRedDot,
+    toggleBadge,
+    setStyle,
+    setItem,
+    toggle,
   };
 }
 
