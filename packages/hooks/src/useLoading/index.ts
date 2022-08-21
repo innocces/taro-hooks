11e3ac0822251a88fd7e999209c3d1b191fe499e
Enside <enside@qq.com>
2022-08-21 21:39:42 +0800

    feat(useLoading): 增强 loading 显隐逻辑

--- packages/hooks/src/useLoading/index.ts
+++ packages/hooks/src/useLoading/index.ts
@@ -1,5 +1,5 @@
-import { showLoading, hideLoading } from '@tarojs/taro';
-import { useCallback, useEffect, useRef } from 'react';
+import { showLoading, hideLoading, getCurrentInstance } from '@tarojs/taro';
+import { useCallback, useEffect, useMemo, useRef } from 'react';
 
 export interface LoadingOption {
   title: string;
@@ -11,10 +11,33 @@ export type ShowLoading = (
 ) => Promise<TaroGeneral.CallbackResult>;
 export type HideLoading = () => Promise<TaroGeneral.CallbackResult>;
 
+const useLoadingPageRecord: Record<string, Set<number> | undefined> = {};
+const USE_LOADING_MAX_COUNT = 0b11111111;
+let useLoadingCount = 0;
+
 function useLoading(
   option?: Partial<LoadingOption>,
 ): [ShowLoading, HideLoading] {
   const initialOption = useRef<Partial<LoadingOption>>();
+  const pageKey = useMemo(
+    () => (getCurrentInstance().page as any)?.$taroPath,
+    [],
+  );
+  const loadingKey = useMemo(() => {
+    useLoadingCount++;
+    if (useLoadingCount > USE_LOADING_MAX_COUNT) {
+      useLoadingCount = 0;
+    }
+    return useLoadingCount;
+  }, []);
+
+  useEffect(() => {
+    useLoadingPageRecord[pageKey] = new Set();
+    return () => {
+      useLoadingPageRecord[pageKey] = undefined;
+      hideLoading();
+    };
+  }, [pageKey]);
 
   useEffect(() => {
     initialOption.current = option;
@@ -22,6 +45,7 @@ function useLoading(
 
   const showLoadingAsync = useCallback<ShowLoading>(
     (option) => {
+      useLoadingPageRecord[pageKey]?.add(loadingKey);
       return new Promise((resolve, reject) => {
         try {
           if (!option && !initialOption.current) {
@@ -48,10 +72,17 @@ function useLoading(
         }
       });
     },
-    [initialOption],
+    [initialOption, loadingKey, pageKey],
   );
 
   const hideLoadingAsync = useCallback<HideLoading>(() => {
+    const pageRecord = useLoadingPageRecord[pageKey];
+    if (pageRecord) {
+      pageRecord.delete(loadingKey);
+      if (pageRecord.size > 0) {
+        return Promise.resolve({ errMsg: 'hideLoading:ok' });
+      }
+    }
     return new Promise((resolve, reject) => {
       try {
         hideLoading({
@@ -62,7 +93,7 @@ function useLoading(
         reject(e);
       }
     });
-  }, []);
+  }, [loadingKey, pageKey]);
 
   return [showLoadingAsync, hideLoadingAsync];
 }
