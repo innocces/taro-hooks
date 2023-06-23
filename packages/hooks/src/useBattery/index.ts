cd324aab8222cf8cbc959208946afd51ee1a3fce
innocces <jiangjiangjiangxi@gmail.com>
2023-06-23 21:21:04 +0800

    fix(type): remove useless type declare

--- packages/hooks/src/useBattery/index.ts
+++ packages/hooks/src/useBattery/index.ts
@@ -1,10 +1,6 @@
 import { getBatteryInfoSync } from '@tarojs/taro';
 import { createUseInfoHook } from '../createUseInfoHook';
 
-export type Result = Taro.getBatteryInfoSync.Result;
-
-type s = Awaited<ReturnType<typeof getBatteryInfoSync>>;
-
 const useBattery = createUseInfoHook<'getBatteryInfoSync'>(
   getBatteryInfoSync,
   {},
