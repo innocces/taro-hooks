fc38fd70b0aa7a5c87d8fae3456d87a137780b53
青木 <31089228+StringKe@users.noreply.github.com>
2023-08-15 02:14:31 +0000

    feat(taro-hooks): add Keyboard hook

---
+++ packages/hooks/src/useKeyboard/index.ts
@@ -0,0 +1,42 @@
+import { useRef, useEffect, useState, useCallback } from '@taro-hooks/core';
+import { hideKeyboard } from '@tarojs/taro';
+import Taro from '@tarojs/taro';
+import usePromise from '../usePromise';
+
+import type { PromiseWithoutOptionAction } from '../type';
+
+export type Close = PromiseWithoutOptionAction<TaroGeneral.CallbackResult>;
+export type SelectedRange =
+  PromiseWithoutOptionAction<TaroGeneral.CallbackResult>;
+
+function useKeyborad() {
+  const [height, setHeight] = useState<number>(0);
+
+  useEffect(() => {
+    const handle = (e: Taro.onKeyboardHeightChange.CallbackResult) => {
+      setHeight(e.height);
+    };
+
+    Taro.onKeyboardHeightChange(handle);
+
+    return () => {
+      Taro.offKeyboardHeightChange(handle);
+    };
+  });
+
+  const handleClose: Close = usePromise<{}, TaroGeneral.CallbackResult>(
+    hideKeyboard,
+  );
+  const getSelectedRange: SelectedRange = usePromise<
+    {},
+    TaroGeneral.CallbackResult
+  >(Taro.getSelectedTextRange);
+
+  return {
+    height,
+    close: handleClose,
+    getRange: getSelectedRange,
+  };
+}
+
+export default useKeyborad;
