da265b60ede2770cd120e45863494ecf2496fc87
innocces <jiangjiangjiangxi@gmail.com>
2024-07-24 18:51:57 +0800

    docs(plugin-vite): addon vite-plugin example

---
+++ examples/taro-plugin-react-vite/global.d.ts
@@ -0,0 +1,31 @@
+/// <reference types="@tarojs/taro" />
+/// <reference types="@taro-hooks/plugin-react" />
+import '@taro-hooks/plugin-react';
+
+declare module '*.png';
+declare module '*.gif';
+declare module '*.jpg';
+declare module '*.jpeg';
+declare module '*.svg';
+declare module '*.css';
+declare module '*.less';
+declare module '*.scss';
+declare module '*.sass';
+declare module '*.styl';
+
+declare const CF: string;
+
+declare namespace NodeJS {
+  interface ProcessEnv {
+    TARO_ENV:
+      | 'weapp'
+      | 'swan'
+      | 'alipay'
+      | 'h5'
+      | 'rn'
+      | 'tt'
+      | 'quickapp'
+      | 'qq'
+      | 'jd';
+  }
+}
---
+++ examples/taro-plugin-react-vite/src/app.ts
@@ -0,0 +1,19 @@
+import { Component } from 'react';
+import type { PropsWithChildren } from 'react';
+
+import './app.less';
+
+class App extends Component<PropsWithChildren> {
+  componentDidMount() {}
+
+  componentDidShow() {}
+
+  componentDidHide() {}
+
+  // this.props.children 是将要会渲染的页面
+  render() {
+    return this.props.children;
+  }
+}
+
+export default App;
